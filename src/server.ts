7e06500d2aa103c925bab1fbd1c02aa0cfd4898f
Mateus Neri de Souza <mtsneri@gmail.com>
2021-02-22 13:22:57 -0300

    add test routes for GET and POST methods - success test

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running!"))
+app.get("/", (request, response) => {
+    return response.json({ message: "Testing GET method" });
+});
+
+app.post("/", (request, response) => {
+    return response.json({ message: "Testing POST method" });
+});
+
+app.listen(3333, () => console.log("Server is running!"));
