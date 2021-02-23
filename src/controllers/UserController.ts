5e9b4fab441447867a24173e7c67ee96e1f31f44
Mateus Neri de Souza <mtsneri@gmail.com>
2021-02-23 10:10:47 -0300

    create users controller and routes file

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,12 @@
-class UserController {
+import { Request, Response } from 'express';
 
+class UserController {
+    async create(request: Request, response: Response) {
+        const body = request.body;
+        console.log(body);
+
+        return response.send();
+    }
 }
 
 export { UserController };
