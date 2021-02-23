f043c2ca355e57c99a4049b9c5dc2c945f9754dd
Mateus Neri de Souza <mtsneri@gmail.com>
2021-02-23 10:19:37 -0300

    implements routers use

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import 'reflect-metadata';
 import './database';
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Testing GET method" });
-});
-
-app.post("/", (request, response) => {
-    return response.json({ message: "Testing POST method" });
-});
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
