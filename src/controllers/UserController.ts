dc2a71078e066775c9f232e271100c685b9dc166
Mateus Neri de Souza <mtsneri@gmail.com>
2021-02-23 11:11:47 -0300

    create repository for users and save data in database

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
     async create(request: Request, response: Response) {
-        const body = request.body;
-        console.log(body);
+        const { name, email } = request.body;
 
-        return response.send();
+        const usersRepository = getRepository(User);
+
+        const user = usersRepository.create({
+            name, email
+        });
+
+        await usersRepository.save(user);
+
+        return response.json(user);
     }
 }
 
