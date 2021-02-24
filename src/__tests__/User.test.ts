42750443175bac741ca3dde8df1a1d29dca9281f
Mateus Neri de Souza <mtsneri@gmail.com>
2021-02-24 20:59:48 -0300

    create survey test - if able to create

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -16,5 +16,13 @@ describe("Users", () => {
             name: "User Example",
         });
         expect(response.status).toBe(201);
-    })
+    });
+
+    it("Should not be able to create a new user with existing email", async () => {
+        const response = await request(app).post("/users").send({
+            email: "user@example.com",
+            name: "User Example",
+        });
+        expect(response.status).toBe(400);
+    });
 });
