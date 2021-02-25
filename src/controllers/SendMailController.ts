69bbc3e6bcd5a5e6c4a6e1f10b09338a8d6296ab
Mateus Neri de Souza <mtsneri@gmail.com>
2021-02-25 16:44:22 -0300

    validate if the survey is being sent more than once

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -31,6 +31,23 @@ class SendMailController {
             });
         }
 
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            user_id: user.id,
+            link: process.env.URL_MAIL,
+        }
+        const npsPath = resolve(__dirname, "../", "views", "emails", "npsMail.hbs");
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: [{ user_id: user.id }, { value: null }]
+        });
+
+        if (surveyUserAlreadyExists) {
+            await SendMailService.execute(email, survey.title, variables, npsPath);
+            return response.json(surveyUserAlreadyExists);
+        }
         // Save informations in surveyUser table
         const surveyUser = surveysUsersRepository.create({
             user_id: user.id,
@@ -39,18 +56,7 @@ class SendMailController {
 
         await surveysUsersRepository.save(surveyUser);
         // Send email to user
-        const npsPath = resolve(__dirname, "../", "views", "emails", "npsMail.hbs");
-
-        const variables = {
-            name: user.name,
-            title: survey.title,
-            description: survey.description,
-            user_id: user.id,
-            link: process.env.URL_MAIL,
-        }
-
         await SendMailService.execute(email, survey.title, variables, npsPath);
-
         return response.json(surveyUser);
 
     }
