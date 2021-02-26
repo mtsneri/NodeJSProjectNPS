662ea1ca049f116414780e60ca49cf8b3ab6470d
Mateus Neri de Souza <mtsneri@gmail.com>
2021-02-26 18:50:11 -0300

    refactor all errors by creating a standard class AppError and using throw statement

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { resolve } from 'path';
 import { getCustomRepository } from "typeorm";
+import { AppError } from "../errors/AppError";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
@@ -26,9 +27,7 @@ class SendMailController {
         const survey = await surveysRepository.findOne({ id: survey_id });
 
         if (!survey) {
-            return response.status(400).json({
-                error: "Survey does not exists",
-            });
+            throw new AppError("Survey does not exists");
         }
 
         const npsPath = resolve(__dirname, "../", "views", "emails", "npsMail.hbs");
